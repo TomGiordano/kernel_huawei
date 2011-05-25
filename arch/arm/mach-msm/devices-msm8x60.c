d647a1551efe1f2e724d0dc010fcd9f50973ab1d
Vasantha Kumar <vasant@codeaurora.org>
2011-05-25 13:07:54 -0700

    Revert "msm: kgsl: Idle calc"
    This reverts commit b0569f579c423adc1779faf8abca96e76924a5e4.
    
    Signed-off-by: Vasantha Kumar <vasant@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/devices-msm8x60.c

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -444,13 +444,18 @@ static struct msm_bus_vectors grp3d_init_vectors[] = {
 };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 static struct msm_bus_vectors grp3d_max_vectors[] = {
 =======
 static struct msm_bus_vectors grp3d_nominal_low_vectors[] = {
+=======
+static struct msm_bus_vectors grp3d_nominal_vectors[] = {
+>>>>>>> debd216... Revert "msm: kgsl: Idle calc"
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
+<<<<<<< HEAD
 		.ib = 1397000000U,
 	},
 };
@@ -462,6 +467,9 @@ static struct msm_bus_vectors grp3d_nominal_high_vectors[] = {
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 2008000000U,
 		.ib = 2008000000U,
+=======
+		.ib = 200800000U,
+>>>>>>> debd216... Revert "msm: kgsl: Idle calc"
 	},
 };
 
@@ -470,7 +478,7 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 2484000000U,
+		.ib = 2008000000U,
 	},
 };
 
@@ -481,6 +489,7 @@ static struct msm_bus_paths grp3d_bus_scale_usecases[] = {
 	},
 	{
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 		ARRAY_SIZE(grp3d_nominal_low_vectors),
 		grp3d_nominal_low_vectors,
@@ -488,6 +497,10 @@ static struct msm_bus_paths grp3d_bus_scale_usecases[] = {
 	{
 		ARRAY_SIZE(grp3d_nominal_high_vectors),
 		grp3d_nominal_high_vectors,
+=======
+		ARRAY_SIZE(grp3d_nominal_vectors),
+		grp3d_nominal_vectors,
+>>>>>>> debd216... Revert "msm: kgsl: Idle calc"
 	},
 	{
 >>>>>>> b0569f5... msm: kgsl: Idle calc
@@ -518,10 +531,14 @@ static struct msm_bus_vectors grp2d0_max_vectors[] = {
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
 <<<<<<< HEAD
+<<<<<<< HEAD
 		.ib = 2096000000U,
 =======
 		.ib = 1397000000U,
 >>>>>>> b0569f5... msm: kgsl: Idle calc
+=======
+		.ib = 248000000,
+>>>>>>> debd216... Revert "msm: kgsl: Idle calc"
 	},
 };
 
@@ -557,10 +574,14 @@ static struct msm_bus_vectors grp2d1_max_vectors[] = {
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
 <<<<<<< HEAD
+<<<<<<< HEAD
 		.ib = 2096000000U,
 =======
 		.ib = 1397000000U,
 >>>>>>> b0569f5... msm: kgsl: Idle calc
+=======
+		.ib = 248000000,
+>>>>>>> debd216... Revert "msm: kgsl: Idle calc"
 	},
 };
 
@@ -660,28 +681,23 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		.pwrlevel = {
 			{
 				.gpu_freq = 266667000,
-				.bus_freq = 3,
-			},
-			{
-				.gpu_freq = 228571000,
 				.bus_freq = 2,
 			},
 			{
-				.gpu_freq = 200000000,
+				.gpu_freq = 228571000,
 				.bus_freq = 1,
 			},
 			{
-				.gpu_freq = 27000000,
+				.gpu_freq = 266667000,
 				.bus_freq = 0,
 			},
 		},
 		.init_level = 0,
-		.num_levels = 4,
+		.num_levels = 3,
 		.set_grp_async = NULL,
 		.idle_timeout = HZ/5,
 #ifdef CONFIG_MSM_BUS_SCALING
 		.nap_allowed = true,
-		.idle_pass = true,
 #endif
 	},
 	.clk = {
@@ -725,11 +741,11 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwr_data = {
 		.pwrlevel = {
 			{
-				.gpu_freq = 200000000,
+				.gpu_freq = 228571000,
 				.bus_freq = 1,
 			},
 			{
-				.gpu_freq = 200000000,
+				.gpu_freq = 228571000,
 				.bus_freq = 0,
 			},
 		},
@@ -810,11 +826,11 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwr_data = {
 		.pwrlevel = {
 			{
-				.gpu_freq = 200000000,
+				.gpu_freq = 228571000,
 				.bus_freq = 1,
 			},
 			{
-				.gpu_freq = 200000000,
+				.gpu_freq = 228571000,
 				.bus_freq = 0,
 			},
 		},
