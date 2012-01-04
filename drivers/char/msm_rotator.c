1ef0f315c70c2cd056618cae462f998d9bc894f0
Adrian Salido-Moreno <adrianm@codeaurora.org>
2012-01-03 18:46:25 -0800

    msm: rotator: Fix rotator YCrCb planar formats unexpected output
    Rotator expects planar input in YCbCr order, wrong color can be seen
    on ouput image when given YCrCb because the chromninance planes picked
    are not correct. Swap Cb and Cr plane addresses to output correct image.
    
    Change-Id: Ia05ab704d18da4aac49e63fb805c40a95ea51a33
    CRs-Fixed: 327956
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    
    Conflicts:
    
    	drivers/char/msm_rotator.c
    
    Conflicts:
    
    	drivers/char/msm_rotator.c

--- drivers/char/msm_rotator.c
+++ drivers/char/msm_rotator.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2009-2012, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -370,6 +370,15 @@ static int msm_rotator_ycxcx_h2v2(struct msm_rotator_img_info *info,
 	bpp = get_bpp(info->src.format);
 	if (bpp < 0)
 		return -ENOTTY;
+
+	/* rotator expects YCbCr for planar input format */
+	if (info->src.format == MDP_Y_CR_CB_H2V2 ||
+	    info->src.format == MDP_Y_CR_CB_GH2V2)
+		swap(in_chroma_paddr, in_chroma2_paddr);
+
+	iowrite32(in_paddr, MSM_ROTATOR_SRCP0_ADDR);
+	iowrite32(in_chroma_paddr, MSM_ROTATOR_SRCP1_ADDR);
+	iowrite32(in_chroma2_paddr, MSM_ROTATOR_SRCP2_ADDR);
 
 	iowrite32(in_paddr, MSM_ROTATOR_SRCP0_ADDR);
 	iowrite32(chroma_addr(in_paddr, info->src.width, info->src.height, bpp),
